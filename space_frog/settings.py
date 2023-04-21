ae6acccd7daf388e43b07cafc93f80278eb0e7b9
Katherine Perdue <krp0047@auburn.edu>
2023-04-21 16:02:15 -0400

    Added parallax background and scrolling

--- space_frog/settings.py
+++ space_frog/settings.py
@@ -1,3 +1,5 @@
-SCREEN_WIDTH = 1024
+SCREEN_WIDTH = 1366
 SCREEN_HEIGHT = 768
+WORLD_WIDTH = 4096 #width of background image
+WORLD_HEIGHT = 4096 #width of background image
 FPS = 30
--- space_frog/space_frog_game.py
+++ space_frog/space_frog_game.py
@@ -8,6 +8,29 @@ from pygame.sprite import Sprite, Group, GroupSingle
 
 import space_frog.settings as S
 
+class Background(Sprite):
+    def __init__(self, base, star1, star2, *groups):
+        super().__init__(*groups)
+        self.bg_images = self.load_background_images(base, star1, star2)
+        self.width = self.bg_images[0].get_width()
+        assert self.width == S.WORLD_WIDTH
+        self.world_rect = self.bg_images[0].get_rect().move(0, 0)
+
+    def load_background_images(self, base, star1, star2):
+        bg_images = []
+        bg_images.append(pygame.image.load(f"space_frog/images/background/{base}.png").convert())
+        bg_images.append(pygame.image.load(f"space_frog/images/background/{star1}.png").convert_alpha())
+        bg_images.append(pygame.image.load(f"space_frog/images/background/{star2}.png").convert_alpha())
+        return bg_images
+
+    def draw(self, screen):
+        speed = 1
+        for i in self.bg_images:
+            screen.blit(i, (self.rect.x * speed, self.rect.y * speed))
+            speed += 0.5
+
+
+
 class Player(Sprite):
     def __init__(self, x, y, *groups):
         super().__init__(*groups)
@@ -15,9 +38,9 @@ class Player(Sprite):
         self.image = pygame.image.load('space_frog/images/frog_frame.png').convert_alpha()
         self.image = pygame.transform.rotate(self.image, angle)
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = self.image.get_rect().move(x, y)
+        self.world_rect = self.image.get_rect().move(x, y)
 
-        self.center = pygame.Vector2(self.rect.center)
+        self.center = pygame.Vector2(self.world_rect.center)
         self.vector = pygame.Vector2()
         self.vector.from_polar((1, angle))
         self.speed = 10
@@ -35,22 +58,22 @@ class Player(Sprite):
             self.vector.rotate_ip(5)
             # self.image = pygame.transform.rotate(self.image, 5)
         self.center += self.vector * delta * self.speed
-        self.rect.center = self.center
+        self.world_rect.center = self.center
 
 class Splat(Sprite):
     def __init__(self, x, y, *groups):
         super().__init__(*groups)
         self.image = Surface((25, 25))
         self.image.fill((0, 100, 25))
-        self.rect = self.image.get_rect().move(x, y)
+        self.world_rect = self.image.get_rect().move(x, y)
 
-        self.center = pygame.Vector2(self.rect.center)
+        self.center = pygame.Vector2(self.world_rect.center)
         self.vector = pygame.Vector2()
         self.speed = 0
 
     def update(self, delta, *args):
         self.center += self.vector * delta * self.speed
-        self.rect.center = self.center
+        self.world_rect.center = self.center
 
 class Asteroid(Sprite):
     def __init__(self, x, y, *groups):
@@ -59,12 +82,12 @@ class Asteroid(Sprite):
         width = self.image.get_rect().width
         height = self.image.get_rect().height
         angle = random.randint(0, 360)
-        self.image = pygame.transform.scale(self.image, (width / random.randint(2, 10), height / random.randint(2, 10)))
+        self.image = pygame.transform.scale(self.image, (width / random.randint(2, 15), height / random.randint(2, 15)))
         self.image = pygame.transform.rotate(self.image, angle)
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = self.image.get_rect().move(x, y)
+        self.world_rect = self.image.get_rect().move(x, y)
 
-        self.center = pygame.Vector2(self.rect.center)
+        self.center = pygame.Vector2(self.world_rect.center)
         self.vector = pygame.Vector2()
         self.vector.from_polar((1, angle))
         self.speed = random.randint(0, 60)
@@ -72,14 +95,26 @@ class Asteroid(Sprite):
 
     def update(self, delta, group):
         self.center += self.vector * delta * self.speed
-        self.rect.center = self.center
+        self.world_rect.center = self.center
 
         if (collided_with := pygame.sprite.spritecollideany(self, group, pygame.sprite.collide_mask)):
             old_vector = self.vector
             self.vector.reflect_ip(collided_with.vector)
             collided_with.vector.reflect_ip(old_vector)
 
-        
+
+class Viewport:
+    def __init__(self):
+        self.left = 0
+        self.top = 0
+
+    def update(self, sprite):
+        self.left = sprite.world_rect.left - (S.SCREEN_WIDTH / 2)
+        self.top = sprite.world_rect.top - (S.SCREEN_HEIGHT / 2)
+
+    def update_rect(self, group):
+         for sprite in group:
+             sprite.rect = sprite.world_rect.move(-self.left, -self.top)       
 
 class Game:
     def __init__(self):
@@ -88,15 +123,23 @@ class Game:
         #     print(m)
         self.screen = pygame.display.set_mode((S.SCREEN_WIDTH, S.SCREEN_HEIGHT))
         pygame.display.set_caption("Space Frog!")
-        self.player = Player(500, 500)
+        self.player = Player(S.SCREEN_WIDTH / 2, S.SCREEN_HEIGHT / 2)
         self.player_group = GroupSingle()
         self.player_group.add(self.player)
         self.asteroids = Group()
+        self.background = Background("blue_nebula", "small_stars_1", "big_stars_1")
+        self.bg_group = Group()
+        self.bg_group.add(self.background)
+        self.viewport = Viewport()
+        self.viewport.update(self.player)
+
         self.clock = pygame.time.Clock()
         self.delta = 0
         self.fps = S.FPS
-        for i in range(7):
-            self.asteroids.add(Asteroid(random.randrange(0, S.SCREEN_WIDTH), random.randrange(0, S.SCREEN_HEIGHT)))
+
+        for i in range(50):
+            self.asteroids.add(Asteroid(random.randrange(0, S.WORLD_WIDTH), random.randrange(0, S.WORLD_HEIGHT))) 
+        
 
     def game_loop(self):
         while True:
@@ -121,6 +164,10 @@ class Game:
     def update(self):
         self.player_group.update(self.delta, key.get_pressed())
         self.asteroids.update(self.delta, self.asteroids)
+        self.viewport.update(self.player)
+        self.check_player_collisions()
+
+    def check_player_collisions(self):
         if self.player.alive() and (collided_with := pygame.sprite.spritecollideany(self.player, self.asteroids, pygame.sprite.collide_mask)):
             if self.player.speed <= 30:
                 # Moving slowly? Land on the asteroid
@@ -142,9 +189,12 @@ class Game:
 
 
     def draw(self):
-        self.screen.fill((15, 15, 15))
+        self.viewport.update_rect(self.bg_group)
+        self.viewport.update_rect(self.player_group)
+        self.viewport.update_rect(self.asteroids)
+        self.background.draw(self.screen)
         self.player_group.draw(self.screen)
-        self.asteroids.draw(self.screen)
+        self.asteroids.draw(self.screen) 
 
 
 if __name__ == "__main__":
